778b3aafad50e7f567be7420487cfc5b29aca407
SciencyBagel <87948191+SciencyBagel@users.noreply.github.com>
2022-04-23 14:08:44 -0500

    activity_main.xml

--- app/src/main/java/com/example/achievav2/MainActivity.kt
+++ app/src/main/java/com/example/achievav2/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var Sex: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_analyzer_water)
 
 
     }
